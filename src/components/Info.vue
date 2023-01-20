31f31cd76ba31a352655475c2022d6d489b0ab32
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-20 00:31:00 +0000

    Aula #09 Diretivas (v-if, v-show)
    Edited:
    Info.vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>JS</li>
       <li>C++</li>
       <li>Python</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
   </div>
 </template>
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Info'
+  name: 'Info',
+  data() {
+    return{
+      esta_trabalhando: true,
+      mostrar_email: true,
+      email: 'vasco.teixeira@gmail.com'
+    }
+  }
 }
 </script>
