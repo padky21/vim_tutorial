c1cf63cfd9b0338b9f144929effe491a41adf3ec
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-22 14:22:26 +0000

    Aula #16 Reutilizacao de componetes
    Edited:
    PrimeiroComponente.vue
    Picture.vue
    
    Added:
    Reutilizacao.vue

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,11 +1,14 @@
 <template>
   <img :src="avatar" :alt="descricao"/>
+  <Reutilizacao/>
 </template>
 
 <script>
+  import Reutilizacao from "@/components/Reutilizacao";
   export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Picture',
+    components: {Reutilizacao},
     data() {
       return{
         avatar: "/img/deb.png",
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
+    <Reutilizacao/>
     <h1>Ola vue!</h1>
+    <Reutilizacao/>
     <p>Meu nome é {{nome}} e trabalho como {{profissao}}</p>
     <MultiplosEventos/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
   </div>
 </template>
 
 <script>
   import MultiplosEventos from './MultiplosEventos.vue'
+  import Reutilizacao from "@/components/Reutilizacao";
   export default {
     name: 'PrimeiroComponente',
     components: {
+      Reutilizacao,
       // eslint-disable-next-line vue/no-unused-components
       MultiplosEventos
     },
