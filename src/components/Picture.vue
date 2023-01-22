52b835636c5204c40a6bb9c3839fffc5d0e50bc6
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-22 14:58:25 +0000

    Aula #18 Emit
    Edited:
    Picture.vue
    
    Added:
    avatar.png
    MudarImagem.vue

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,18 +1,23 @@
 <template>
   <img :src="avatar" :alt="descricao"/>
-  <Reutilizacao/>
+  <MudarImagem @mudar-imagem="trocarImagem" />
 </template>
 
 <script>
-  import Reutilizacao from "@/components/Reutilizacao";
+  import MudarImagem from "@/components/MudarImagem";
   export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Picture',
-    components: {Reutilizacao},
+    components: {MudarImagem},
     data() {
       return{
         avatar: "/img/deb.png",
         descricao: "Vasco Teixeira"
+      }
+    },
+    methods: {
+      trocarImagem(){
+        this.avatar = "/img/avatar.png"
       }
     }
   }
