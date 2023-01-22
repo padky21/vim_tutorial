1bf4d06f2a1e047c51e7fbf70c689b3c245fe62b
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-22 14:02:40 +0000

    Aula #14 Eventos @submit @click
    Edited:
    Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,19 +1,45 @@
 <template>
-  <form action="">
-    <InputText/>
-    <input-text/>
+  <form action="" @submit="enviarFormulario($event)">
+    <input type="text" v-model="name"/>
+    <input type="text" v-model="email"/>
     <submit/>
   </form>
+  <p>Conteudo</p>
+  <p>Conteudo</p>
+  <p>Conteudo</p>
 </template>
 <script>
-  import InputText from "@/components/Form/InputText";
   import Submit from "@/components/Form/Submit";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Form',
   components: {
-    InputText,
     Submit
+  },
+  data(){
+    return{
+      name: '',
+      email: ''
+    }
+  },
+  methods: {
+    enviarFormulario(e){
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      console.log("Form enviado");
+      console.log("O nome é: " + name);
+      console.log("O email é: " + email);
+
+    }
   }
 }
 </script>
+
+<style scoped>
+p{
+  margin: 150px;
+}
+</style>
