643795820c306557e17a57a91f459ff711ccd6d5
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-22 14:15:51 +0000

    Aula #15 Multiplos eventos
    Edited:
    PrimeiroComponente.vue
    
    Added:
    MultiplosEventos.vue

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,18 @@
   <div>
     <h1>Ola vue!</h1>
     <p>Meu nome é {{nome}} e trabalho como {{profissao}}</p>
+    <MultiplosEventos/>
   </div>
 </template>
 
 <script>
+  import MultiplosEventos from './MultiplosEventos.vue'
   export default {
     name: 'PrimeiroComponente',
+    components: {
+      // eslint-disable-next-line vue/no-unused-components
+      MultiplosEventos
+    },
     data(){
       return{
         nome: "Vasco",
