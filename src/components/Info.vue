8fa2db8169b5edcd8a6e83ba88af98d645044f65
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2023-01-22 13:47:29 +0000

    Aula #13 Renderização de listas usando v-for
    Edited:
    Info.vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,21 +3,26 @@
     <Picture/>
     <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
     <p v-else>Estou em busca de novas oportunidades</p>
-    <p>Utilizo as seguintes tecnologias:</p>
+    <p>Utilizo as seguintes tecnologias para backend:</p>
     <ul>
-      <li>JS</li>
-      <li>C++</li>
-      <li>Python</li>
+      <li v-for="(technology, index) in backend_tech" v-bind:key="index">{{ technology }}</li>
+    </ul>
+    <p>Utilizo as seguintes tecnologias para frontend:</p>
+    <ul>
+      <li v-for="technology in frontend_tech" :key="technology.id">
+        {{ technology.language }}
+      </li>
     </ul>
     <div>
-      <button @click="showEmail">{{textoBotao}}</button>
+      <button @click="showEmail">{{ textoBotao }}</button>
     </div>
-    <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     <p class="teste">Para acessar meu portfólio <a target="_blank" v-bind:href="meu_link">Clique aqui</a></p>
   </div>
 </template>
 <script>
 import Picture from "@/components/Picture";
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Info',
@@ -25,20 +30,26 @@ export default {
     Picture
   },
   data() {
-    return{
+    return {
       esta_trabalhando: true,
       mostrar_email: false,
       textoBotao: 'Mostrar e-mail',
       email: 'vasco.teixeira@gmail.com',
-      meu_link:'https://google.com'
+      meu_link: 'https://google.com',
+      backend_tech: ['JavaScript', 'PHP', 'Python'],
+      frontend_tech: [
+        {id: 1, language: 'HTML'},
+        {id: 2, language: 'CSS'},
+        {id: 3, language: 'VUE'}
+      ]
     }
   },
   methods: {
     showEmail() {
       this.mostrar_email = !this.mostrar_email
-      if(!this.mostrar_email){
+      if (!this.mostrar_email) {
         this.textoBotao = 'Mostrar e-mail'
-      }else{
+      } else {
         this.textoBotao = 'Esconder e-mail'
       }
     }
@@ -47,7 +58,7 @@ export default {
 </script>
 
 <style>
-.paragrafo-pai{
+.paragrafo-pai {
   color: red;
 }
 </style>
